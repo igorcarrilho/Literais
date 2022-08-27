e2e0582093c41a246ebbbc562e7bfe41181bde1c
Igor <igor_g_carrilho@hotmail.com>
2022-08-27 18:34:44 -0300

    Literais em Java

---
+++ src/Literal.java
@@ -0,0 +1,18 @@
+public class Literal {
+    public static void main(String[] args){
+        int numero;
+        String cadeiaCaractere;
+        char caractere;
+        boolean certoOuErrado;
+
+        numero = 10;
+        cadeiaCaractere = "Igor Carrilho";
+        caractere = 's';
+        certoOuErrado = true;
+
+        System.out.println("O valor de número é: " + numero);
+        System.out.println("O valor de cadeiaCaractere é: " + cadeiaCaractere);
+        System.out.println("O valor de caractere é: " + caractere);
+        System.out.println("O valor de certoOuErrado é: " + certoOuErrado);
+    }
+}
